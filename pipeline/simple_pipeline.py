a2380375f4eafb839af2a3e40a8d682807b252d9
Chris Heckler <hecklerchris@hotmail.com>
2018-02-19 11:45:34 -0800

    Stage 2 complete

--- pipeline/simple_pipeline.py
+++ pipeline/simple_pipeline.py
@@ -18,7 +18,7 @@ class PrintNumbers(luigi.Task):
         """
         return luigi.LocalTarget('numbers_up_to_{}.txt'.format(self.n))
 
-    def run():
+    def run(self):
         """
         The part that is exectuted to accomplish or transform data
         """
@@ -26,7 +26,21 @@ class PrintNumbers(luigi.Task):
             for i in range(1, self.n+1):
                 f.write("{}\n".format(i))
 
-#class SquareNumbers(luigi.Task):
+class SquareNumbers(luigi.Task):
+    n = luigi.IntParameter(default=20)
+
+    def requires(self):
+        return [PrintNumbers(n=self.n)]
+    
+    def output(self):
+        return luigi.LocalTarget("sqaures_up_to_{}.txt".format(self.n))
+
+    def run(self):
+        with self.input()[0].open() as fin, self.output().open('w') as fout:
+            for line in fin:
+                n = int(line.strip())
+                out = n * n
+                fout.write("{}:{}\n".format(n, out))
 
 
 if __name__=="__main__":
